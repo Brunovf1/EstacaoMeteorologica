8d1e5db192a5639b3fe4747a12c33ac4147b6fcb
Bruno Victor <brunovf1@gmail.com>
2016-02-15 12:53:33 -0300

    Yun version

--- EstacaoMeteorologica.ino
+++ EstacaoMeteorologica.ino
@@ -1,22 +1,15 @@
 //EstacaoMeteorologicaV1.ino
-// 05/05/2015
-// Bruno Victor de M. Ferreira  (brunovf1 at gmail dot com)
+// 29/05/2015
 /*
-V1.0
 Estação Meteorologica Simples utilizando Shield SD SparFun,
 Sensor de temperatura e humidade DHT11, Sensor de Temperatura DS18B20,
 e uma placa solar com um divisor de tensao.
 
-V1.1
-Adicionado a campacidade de ajuste da hora em tempo de execuçao
-para tal e necessario enviar via serial uma String com o seguinte formato:
-"T"+timestamp
-
 O log dos dados e salvo no arquivo log.txt, com os seguintes dados:
-data 				==> dia/mes/ano|hora:min:seg
+data 				==> "%Y%m%dT%H%M%SZ"
 temperatura do sensor DHT11 	==> celsius
 temperatura do sensor DS18B20 	==> celsius
-humidade do sensor DHT11 	==> humidade relativa do ar
+humidade do sensor DHT11        ==> humidade relativa do ar
 luminosidade 			==> Valor de tensao da placa solar
 
 As conexoes sao as seguintes:
@@ -27,127 +20,109 @@ pino 5	==> DS18B20
 pino 8	==> Led de erro
 */
 #include <dht.h>
-#include <DS1307RTC.h>  // a basic DS1307 library that returns time as a time_t
 #include <OneWire.h>
-#include <Time.h>  
-#include <Wire.h>  
-#include <SD.h>
-#include <SPI.h>
+#include <FileIO.h>
+#include "Wire.h"
+#include "Adafruit_BMP085.h"
 
 #define dht_dpin A1 //Pino DATA do Sensor DHT11 na porta Analogica A1
+#define SensorPin 5
+#define LumidPin A0
 
+Adafruit_BMP085 bmp;
 dht DHT; //Inicializa o sensor
-byte SensorPin = 5; //pino do sensor de temperatura
-byte LumidPin = A0; //pino da placa solar
 OneWire ds(SensorPin); //objeto do sensor de temperatura
-File myFile;
 
 void setup() {
-	Serial.begin(9600);// Serial para debug
-	Wire.begin();// porta i2c para o RTC
-	pinMode(4, OUTPUT); //pino de controle do cartão SD no baramento ISP
-	pinMode(8,OUTPUT); // pino indicando erro na inicializacao do cartao SD
-	setSyncProvider(RTC.get); // funcao para configurar a biblioteca Time
-	if(timeStatus()!= timeSet){
-//		Serial.println("Unable to sync with the RTC");
-		while(1){ //loop de erro de inicializacao de hora
-			digitalWrite(8,HIGH);
-			delay(100);
-			digitalWrite(8, LOW);
-			delay(100);
-		}
-	}
-	else{
-//		Serial.println("RTC has set the system time");
-	}
-
-	if (!SD.begin(4)) {// verificaçao dd cartao SD
-//		Serial.println("initialization failed!");
-		while(1){// loop de erro
-			digitalWrite(8,HIGH);
-			delay(100);
-			digitalWrite(8, LOW);
-			delay(100);
-		}
-		
-	}
-	myFile = SD.open("Log.txt", FILE_WRITE);// abrir arquivo de log
-	
-	if (myFile) {// aquivo aberto corretamente
-		myFile.println("Date, DHTTemp, DSTemp, Humd, Lumid");//cabeçalho
-		myFile.close();
-	}
-
-	delay(1000);//Aguarda 1 seg antes de acessar as informações do sensor   
+  Wire.begin();// porta i2c para o RTC
+  bmp.begin();
+  Bridge.begin();
+  FileSystem.begin();
+  writeHeader();
+  delay(1000);//Aguarda 1 seg antes de acessar as informações do sensor   
 }
 
+void writeHeader(){
+  String HEADER = "Date, DHTTemp, DSTemp, BMPTemp, Humd, Lumid, Pressure";
+  String fbuffer = getFileName();
+  char fileName[fbuffer.length()+1];
+  fbuffer.toCharArray(fileName,fbuffer.length()+1);
+  File dataFile = FileSystem.open(fileName, FILE_APPEND);
+  if (dataFile) {
+    dataFile.println(HEADER);        
+  }  
+}
 
 void loop() {
-	int readlumid = analogRead(LumidPin);//leituta da placa solar
-	float lumid = 3*readlumid * (5.0 / 1023.0);// conversao para Tensao
-	float ds_temp = getDSTemp();// coleta da temperatura
-	DHT.read11(dht_dpin); //Lê as informações do sensor de temp e humd
-	time_t time_n = now();// captura da hora
-	String now_s = String(day(time_n))+'/'+String(month(time_n));
-	now_s += '/'+String(year(time_n))+'|'+String(hour(time_n))+':';
-	now_s += String(minute(time_n))+':'+String(second(time_n));//converçao de time para String
-//	Serial.println(now_s);//begin debug
-//	Serial.print(',');
-//	Serial.print(ds_temp);
-//	Serial.print(',');
-//	Serial.print(DHT.temperature);
-//	Serial.print(',');
-//	Serial.print(DHT.humidity);
-//	Serial.print(',');
-//	Serial.print(lumid);
-//	Serial.println(';');//end debug
-	datatoCSV(now_s, ds_temp, DHT.temperature, DHT.humidity, lumid);//escita no cartao
-	delay(1000);//delay de leituras
-}
-
-#define TIME_MSG_LEN  11   // time sync to PC is HEADER followed by unix time_t as ten ascii digits
-#define TIME_HEADER  'T'   // Header tag for serial time sync message
-
-void serialEvent() {
-  while(Serial.available() >=  TIME_MSG_LEN ){  // time message consists of a header and ten ascii digits
-    char c = Serial.read() ; 
-//    Serial.print(c); 
-    if( c == TIME_HEADER ) {       
-      time_t pctime = 0;
-      for(int i=0; i < TIME_MSG_LEN -1; i++){   
-        c = Serial.read();          
-        if( c >= '0' && c <= '9'){   
-          pctime = (10 * pctime) + (c - '0') ; // convert digits to a numb
-        }
-      }   
-//      Serial.println(pctime);
-      RTC.set( (time_t) pctime);
-      setTime( (time_t) pctime);
-    }  
+  DHT.read11(dht_dpin); //Lê as informações do sensor de temp e humd
+  String dataString;
+  dataString += getTimeStamp();
+  dataString += String(',');
+  dataString += String(DHT.temperature);
+  dataString += String(',');
+  dataString += String(getDSTemp());
+  dataString += String(',');
+  dataString += String(bmp.readTemperature());
+  dataString += String(',');
+  dataString += String(bmp.readPressure());
+  dataString += String(',');
+  dataString += String(DHT.humidity);
+  dataString += String(',');
+  dataString += String((3*(analogRead(LumidPin)) * (5.0 / 1023.0)));
+  String fbuffer = getFileName();
+  char fileName[fbuffer.length()+1];
+  fbuffer.toCharArray(fileName,fbuffer.length()+1);
+  File dataFile = FileSystem.open(fileName, FILE_APPEND);
+  
+  if (dataFile) {
+    dataFile.println(dataString);
+    dataFile.close();
   }
+  
+  delay(1000);//delay de leituras
 }
 
-void datatoCSV(String now_s, float ds_temp, int dht_temp, int dht_humidity, float lumid){
-	myFile = SD.open("Log.txt", FILE_WRITE);//abre o arquivo
-	if (myFile) {//se abrio o arquivo corretamente
-//		Serial.print("Writing...");
-		myFile.print(now_s);
-		myFile.print(",");
-		myFile.print(ds_temp);
-		myFile.print(",");
-		myFile.print(dht_temp);
-		myFile.print(",");
-		myFile.print(dht_humidity);
-		myFile.print(",");
-		myFile.print(lumid);
-		myFile.println(";");
-		myFile.close();
-//		Serial.println("Done");
-	}
-	else{
-//		Serial.print("Error!");//debug
-	}
+String getTimeStamp(){
+  //date +"%Y%m%dT%H%M%SZ"
+  String result;
+  Process time;
+  // date is a command line utility to get the date and the time
+  // in different formats depending on the additional parameter
+  time.begin("date");
+  time.addParameter("-u");
+  time.addParameter("+%Y%m%dT%H%M%SZ");
+  time.run();  // run the command
+
+  // read the output of the command
+  while (time.available() > 0) {
+    char c = time.read();
+    if (c != '\n')
+      result += c;
+  }
+
+  return result;
 }
+
+String getFileName(){
+  //date +%Y.%m.%d-Estacao-666.csv
+  String result;
+  Process time;
+  // date is a command line utility to get the date and the time
+  // in different formats depending on the additional parameter
+  time.begin("date");
+  time.addParameter("+/mnt/sd/%Y.%m.%d-Estacao-666.csv");
+  time.run();  // run the command
+
+  // read the output of the command
+  while (time.available() > 0) {
+    char c = time.read();
+    if (c != '\n')
+      result += c;
+  }
+
+  return result;
+}
+
 
 float getDSTemp(){ // funçao para ler o sensor de temperatura
 	byte data[12];
@@ -158,11 +133,9 @@ float getDSTemp(){ // funçao para ler o sensor de temperatura
 		return -1000;
 	}
 	if ( OneWire::crc8( addr, 7) != addr[7]) {
-		Serial.println("CRC is not valid!");
 		return -1000;
 	}
 	if ( addr[0] != 0x10 && addr[0] != 0x28) {
-		Serial.print("Device is not recognized");
 		return -1000;
 	}
 	ds.reset();
